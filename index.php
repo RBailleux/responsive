fbc8394209fcd6843aac8ae9814c8488cd256883
robin <robin@Robin-Workstation>
2017-02-09 12:00:17 +0100

    star truc

--- index.php
+++ index.php
@@ -4,29 +4,226 @@
     <meta charset='utf-8'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, user-scalable=no">
-    <title>Responsive</title>
+    <title>Star truc</title>
     <link rel='stylesheet' href='styles/style.css'>
   </head>
   <body>
-   	<div class="container">
-   		<div class="boite">1</div>
-   		<div class="boite">2</div>
-   		<div class="boite">3</div>
-   		<div class="boite">4</div>
-   		<div class="boite">5</div>
-   		<div class="boite">6</div>
-   		<div class="boite">7</div>
-   		<div class="boite">8</div>
-   		<div class="boite">9</div>
-   		<div class="boite">10</div>
-   		<div class="boite">11</div>
-   		<div class="boite">12</div>
-   		<div class="boite">13</div>
-   		<div class="boite">14</div>
-   		<div class="boite">15</div>
-   		<div class="boite">16</div>
-   		<div class="boite">17</div>
-   		<div class="boite">18</div>
-   	</div>
+  	<header>
+  	
+  	</header>
+  	<section id="content">
+  		<div id="intro">
+  			<h1>Lorem ipsum</h1>
+  			<p class="intro-text">
+  				Victus universis caro ferina est lactisque abundans copia qua sustentantur, 
+  				et herbae multiplices et siquae alites capi per aucupium possint, et plerosque mos 
+  				vidimus frumenti usum et vini penitus ignorantes.
+				Quam quidem partem accusationis admiratus sum et moleste tuli potissimum esse Atratino datam. 
+				Neque enim decebat neque aetas illa postulabat neque, id quod animadvertere poteratis, 
+				pudor patiebatur optimi adulescentis in tali illum oratione versari. Vellem aliquis ex 
+				vobis robustioribus hunc male dicendi locum suscepisset; aliquanto liberius et fortius et
+				magis more nostro refutaremus istam male dicendi licentiam. Tecum, Atratine, agam lenius, 
+				quod et pudor tuus moderatur orationi meae et meum erga te parentemque tuum beneficium tueri debeo.
+				Siquis enim militarium vel honoratorum aut nobilis inter suos rumore tenus esset insimulatus fovisse 
+				partes hostiles, iniecto onere catenarum in modum beluae trahebatur et inimico urgente vel 
+				nullo, quasi sufficiente hoc solo, quod nominatus esset aut delatus aut postulatus, 
+				capite vel multatione bonorum aut insulari solitudine damnabatur.
+  			</p>
+  		</div>
+  		<div class="row-img">
+  			<div class="img team1">
+  			
+  			</div>
+  			<div class="text">
+  				<p>
+  					Consurgentem terrebat distarent tutela rati et intempestivum cum distarent bene poterat locari et qui alacriter ductores scuta tutela habitus poterat subire iam tutela pertinax concitat miles locari parans solido scuta.
+  				</p>
+  			</div>
+  		</div>
+  		<div class="row-img">
+  			<div class="img espace">
+  			
+  			</div>
+  			<div class="text">
+  				<p>
+  					Consurgentem terrebat distarent tutela rati et intempestivum cum distarent bene poterat locari et qui alacriter ductores scuta tutela habitus poterat subire iam tutela pertinax concitat miles locari parans solido scuta.
+  				</p>
+  			</div>
+  		</div>
+  		<div class="row-img">
+  			<div class="img team">
+  			
+  			</div>
+  			<div class="text">
+  				<p>
+  					Consurgentem terrebat distarent tutela rati et intempestivum cum distarent bene poterat locari et qui alacriter ductores scuta tutela habitus poterat subire iam tutela pertinax concitat miles locari parans solido scuta.
+  				</p>
+  			</div>
+  		</div>
+  		
+  		<div id="team">
+  			<div class="intro">
+  				<h2>Team</h2>
+  				<p>
+  					Duce rettulimus cupiebat pileo cuius artibus populatam suffragatione reus firmum suum imperium populatam Phoenice
+  				</p>
+  			</div>
+  			<ul class="team-list">
+  				<li>
+  					<div class="team-member">
+  						<div class="photo">
+  							<img src="styles/images/photos/img_site_2_0001_image_16.png">
+  						</div>
+  						<p class="name">
+  							John Doe
+  						</p>
+  					</div>
+  				</li>
+  				<li>
+  					<div class="team-member">
+  						<div class="photo">
+  							<img src="styles/images/photos/img_site_2_0002_image_15.png">
+  						</div>
+  						<p class="name">
+  							John Doe
+  						</p>
+  					</div>
+  				</li>
+  				<li>
+  					<div class="team-member">
+  						<div class="photo">
+  							<img src="styles/images/photos/img_site_2_0003_image_14.png">
+  						</div>
+  						<p class="name">
+  							John Doe
+  						</p>
+  					</div>
+  				</li>
+  				<li>
+  					<div class="team-member">
+  						<div class="photo">
+  							<img src="styles/images/photos/img_site_2_0004_image_13.png">
+  						</div>
+  						<p class="name">
+  							John Doe
+  						</p>
+  					</div>
+  				</li>
+  				<li>
+  					<div class="team-member">
+  						<div class="photo">
+  							<img src="styles/images/photos/img_site_2_0005_image_12.png">
+  						</div>
+  						<p class="name">
+  							John Doe
+  						</p>
+  					</div>
+  				</li>
+  				<li>
+  					<div class="team-member">
+  						<div class="photo">
+  							<img src="styles/images/photos/img_site_2_0006_image_11.png">
+  						</div>
+  						<p class="name">
+  							John Doe
+  						</p>
+  					</div>
+  				</li>
+  				<li>
+  					<div class="team-member">
+  						<div class="photo">
+  							<img src="styles/images/photos/img_site_2_0007_image_10.png">
+  						</div>
+  						<p class="name">
+  							John Doe
+  						</p>
+  					</div>
+  				</li>
+  				<li>
+  					<div class="team-member">
+  						<div class="photo">
+  							<img src="styles/images/photos/img_site_2_0008_image_08.png">
+  						</div>
+  						<p class="name">
+  							John Doe
+  						</p>
+  					</div>
+  				</li>
+  				<li>
+  					<div class="team-member">
+  						<div class="photo">
+  							<img src="styles/images/photos/img_site_2_0009_image_07.png">
+  						</div>
+  						<p class="name">
+  							John Doe
+  						</p>
+  					</div>
+  				</li>
+  				<li>
+  					<div class="team-member">
+  						<div class="photo">
+  							<img src="styles/images/photos/img_site_2_0010_image_06.png">
+  						</div>
+  						<p class="name">
+  							John Doe
+  						</p>
+  					</div>
+  				</li>
+  				<li>
+  					<div class="team-member">
+  						<div class="photo">
+  							<img src="styles/images/photos/img_site_2_0011_image_05.png">
+  						</div>
+  						<p class="name">
+  							John Doe
+  						</p>
+  					</div>
+  				</li>
+  				<li>
+  					<div class="team-member">
+  						<div class="photo">
+  							<img src="styles/images/photos/img_site_2_0012_image_04.png">
+  						</div>
+  						<p class="name">
+  							John Doe
+  						</p>
+  					</div>
+  				</li>
+  				<li>
+  					<div class="team-member">
+  						<div class="photo">
+  							<img src="styles/images/photos/img_site_2_0013_image_03.png">
+  						</div>
+  						<p class="name">
+  							John Doe
+  						</p>
+  					</div>
+  				</li>
+  				<li>
+  					<div class="team-member">
+  						<div class="photo">
+  							<img src="styles/images/photos/img_site_2_0014_image_02.png">
+  						</div>
+  						<p class="name">
+  							John Doe
+  						</p>
+  					</div>
+  				</li>
+  				<li>
+  					<div class="team-member">
+  						<div class="photo">
+  							<img src="styles/images/photos/img_site_2_0015_image_01.png">
+  						</div>
+  						<p class="name">
+  							John Doe
+  						</p>
+  					</div>
+  				</li>
+  			</ul>
+  		</div>
+  	</section>
+  	<footer>
+  		
+  	</footer>
   </body>
 </html>
